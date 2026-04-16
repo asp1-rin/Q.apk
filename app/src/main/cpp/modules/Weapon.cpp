7943d425fa07bb53e8d3935efef4fcd8c7cf379a
asp1_rin <acorn0757@gmail.com>
2026-04-16 17:35:08 +0900

    Update Weapon.cpp

--- app/src/main/cpp/modules/Weapon.cpp
+++ app/src/main/cpp/modules/Weapon.cpp
@@ -4,17 +4,17 @@
 KittyMemory::MemoryPatch recoilPatch;
 KittyMemory::MemoryPatch spreadPatch;
 
-void initWeaponPatches() {
+void SetupWeapon() {
     recoilPatch = KittyMemory::MemoryPatch::buildPatch(G_LIB, 0x33BFAF0, "\x00\x40\x20\x1E", 4);
     spreadPatch = KittyMemory::MemoryPatch::buildPatch(G_LIB, 0x35ADC9C, "\x00\x40\x20\x1E", 4);
 }
 
-void setWeaponPatch(bool enable) {
-    if (enable) {
-        recoilPatch.modify();
-        spreadPatch.modify();
-    } else {
-        recoilPatch.restore();
-        spreadPatch.restore();
-    }
+void ToggleNoRecoil(bool enable) {
+    if (enable) recoilPatch.modify();
+    else recoilPatch.restore();
+}
+
+void ToggleNoSpread(bool enable) {
+    if (enable) spreadPatch.modify();
+    else spreadPatch.restore();
 }

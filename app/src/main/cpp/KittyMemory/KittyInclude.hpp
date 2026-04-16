3cda861a83fbd2a8d978e632cafc872df3ca93ea
asp1_rin <acorn0757@gmail.com>
2026-04-16 13:42:46 +0900

    Add KittyInclude.hpp header file

---
+++ app/src/main/cpp/KittyMemory/KittyInclude.hpp
@@ -0,0 +1,14 @@
+#ifndef KITTYINCLUDE_HPP
+#define KITTYINCLUDE_HPP
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <unistd.h>
+#include <sys/mman.h>
+
+#include "KittyMemory.hpp"
+#include "KittyScanner.hpp"
+#include "MemoryPatch.hpp"
+
+#endif

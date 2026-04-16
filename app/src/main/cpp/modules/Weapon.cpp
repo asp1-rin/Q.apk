311b17cf9bb0b68422fc5b3061838f24ccdce4d2
asp1_rin <acorn0757@gmail.com>
2026-04-16 17:28:00 +0900

    Remove unused G_LIB declaration in Weapon.cpp
    Removed the declaration of G_LIB as it was not used.

--- app/src/main/cpp/modules/Weapon.cpp
+++ app/src/main/cpp/modules/Weapon.cpp
@@ -5,8 +5,6 @@ KittyMemory::MemoryPatch recoilPatch;
 KittyMemory::MemoryPatch spreadPatch;
 
 void initWeaponPatches() {
-    const char* G_LIB = "libUE4.so";
-
     recoilPatch = KittyMemory::MemoryPatch::buildPatch(G_LIB, 0x33BFAF0, "\x00\x40\x20\x1E", 4);
     spreadPatch = KittyMemory::MemoryPatch::buildPatch(G_LIB, 0x35ADC9C, "\x00\x40\x20\x1E", 4);
 }

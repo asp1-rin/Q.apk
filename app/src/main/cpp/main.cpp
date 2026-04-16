97b8b79f57ec836a930ba3da43ecd8684b3e1ff9
asp1_rin <acorn0757@gmail.com>
2026-04-16 15:15:05 +0900

    Update main.cpp

--- app/src/main/cpp/main.cpp
+++ app/src/main/cpp/main.cpp
@@ -5,38 +5,30 @@
 #include "KittyInclude.hpp"
 #include "offsets.h"
 
-// 외부 모듈 함수 연결
 extern void ApplyBypass();
 extern void SetupWeapon();
 extern void ToggleNoRecoil(bool enable);
 extern void ToggleNoSpread(bool enable);
 extern void UpdateAimbot(uintptr_t localPlayer, uintptr_t enemyList[], int enemyCount);
 
-// 전역 변수
 uintptr_t g_LocalPlayer = 0;
-extern bool isAimbotEnabled; // Aimbot.cpp에 정의됨
+bool isAimbotEnabled = false;
 
-// 메인 핵 루프
 void* HackMainThread(void*) {
     ProcMap gameMap;
-
-    // 보안 우회 및 라이브러리 대기
     ApplyBypass();
     do {
         gameMap = KittyMemory::getLibraryMap(G_LIB);
         usleep(500000);
     } while (!gameMap.isValid());
 
-    // 기능 초기화
     SetupWeapon();
 
     while (true) {
-        // 자동 탐색: position-base (0x7ABE28) 활용
         uintptr_t posBaseAddr = gameMap.startAddress + 0x7ABE28;
         g_LocalPlayer = *(uintptr_t*)posBaseAddr;
 
         if (g_LocalPlayer != 0 && isAimbotEnabled) {
-            // 에임봇 업데이트 (적 리스트 로직은 추후 보완)
             UpdateAimbot(g_LocalPlayer, nullptr, 0);
         }
         usleep(10000);
@@ -51,7 +43,6 @@ JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM* vm, void* reserved) {
 }
 
 extern "C" {
-    // JNI 함수 이름이 패키지명 com.internal.asp1rin.launcher에 맞춰 수정됨
     JNIEXPORT void JNICALL
     Java_com_internal_asp1rin_launcher_MainActivity_setNoRecoil(JNIEnv* env, jobject thiz, jboolean enable) {
         ToggleNoRecoil(enable);
@@ -65,5 +56,10 @@ extern "C" {
     JNIEXPORT void JNICALL
     Java_com_internal_asp1rin_launcher_MainActivity_setAimbotState(JNIEnv* env, jobject thiz, jboolean enable) {
         isAimbotEnabled = enable;
+    }
+
+    JNIEXPORT void JNICALL
+    Java_com_internal_asp1rin_launcher_MainActivity_setAimbotFov(JNIEnv* env, jobject thiz, jfloat fov) {
+        // Aimbot.cpp의 fov 변수와 연결
     }
 }

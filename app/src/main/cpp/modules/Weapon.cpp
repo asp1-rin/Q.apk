f325116c73c2b17a77d5008166732dd39709757f
asp1_rin <acorn0757@gmail.com>
2026-04-16 13:57:50 +0900

    Implement no spread patch and toggle function
    Added a new patch for spread control and a toggle function.

--- app/src/main/cpp/modules/Weapon.cpp
+++ app/src/main/cpp/modules/Weapon.cpp
@@ -1,17 +1,26 @@
 #include "offsets.h"
+#include "KittyInclude.hpp"
 
-// 패치 객체 생성
+// 패치 객체들을 전역으로 선언하여 상태 유지
 KittyMemory::MemoryPatch recoilPatch;
+KittyMemory::MemoryPatch spreadPatch;
 
+// 초기 설정: 게임 라이브러리 로드 후 주소와 패치값 준비
 void SetupWeapon() {
-    ProcMap gameMap = KittyMemory::getLibraryMap(G_LIB);
-    if(gameMap.isValid()) {
-        // 주소에 fmov s0, wzr (0x1E204000) 패치 준비
-        recoilPatch = KittyMemory::buildPatch(G_LIB, offsets.no_recoil, "\x00\x40\x20\x1E", 4);
-    }
+    // 무반동 패치 준비 (fmov s0, wzr)
+    recoilPatch = KittyMemory::buildPatch(G_LIB, 0x33BFAF0, "\x00\x40\x20\x1E", 4);
+    
+    // 탄퍼짐 제거 패치 준비 (fmov s0, wzr)
+    spreadPatch = KittyMemory::buildPatch(G_LIB, 0x35ADC9C, "\x00\x40\x20\x1E", 4);
 }
 
+// UI 스위치와 연결될 토글 함수
 void ToggleNoRecoil(bool enable) {
-    if (enable) recoilPatch.modify(); // 패치 적용
-    else recoilPatch.restore();      // 원본 복구
+    if (enable) recoilPatch.modify();
+    else recoilPatch.restore();
+}
+
+void ToggleNoSpread(bool enable) {
+    if (enable) spreadPatch.modify();
+    else spreadPatch.restore();
 }
